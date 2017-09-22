67ce568d7301b2ea6341a28db51cf3fb9ef035fe
Hugo Ashmore <hugo@nettropia.co.uk>
2017-09-22 16:08:29 +0100

    Revert BP customizer files to 'commmunity/includes/' dir. Rename main site postmessage support file for clarity.

---
+++ inc/customizer-postmessage-support.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * BP Progenitor Theme Customizer
+ *
+ * @package BP_Progenitor
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ *
+ * @since 0.1.0
+ */
+function bp_progenitor_main_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial( 'blogname', array(
+			'selector'        => '.site-title a',
+			'render_callback' => 'bp_progenitor_customize_partial_blogname',
+		) );
+		$wp_customize->selective_refresh->add_partial( 'blogdescription', array(
+			'selector'        => '.site-description',
+			'render_callback' => 'bp_progenitor_customize_partial_blogdescription',
+		) );
+	}
+}
+add_action( 'customize_register', 'bp_progenitor_main_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function bp_progenitor_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function bp_progenitor_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function bp_progenitor_customize_preview_js() {
+	wp_enqueue_script( 'bp-progenitor-main-customizer', get_template_directory_uri() . '/assets/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'bp_progenitor_customize_preview_js' );
