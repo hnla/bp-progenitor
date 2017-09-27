a8976a66cc6ad7a1ff022fd869ec838dc9b25adf
Hugo Ashmore <hugo@nettropia.co.uk>
2017-09-27 18:40:16 +0100

    Add new foot widget files: template include. scss partials.

---
+++ template-parts/footer-widgets.php
@@ -0,0 +1,39 @@
+<?php
+/**
+* Site footer widgets block
+*
+* @package BP_Progenitor
+*/
+?>
+
+<?php if ( is_active_sidebar( 'first-footer-widget-area' ) ) : ?>
+				<div id="first" class="widget-area first">
+
+						<?php dynamic_sidebar( 'first-footer-widget-area' ); ?>
+
+				</div><!-- #first .widget-area -->
+<?php endif; ?>
+
+<?php if ( is_active_sidebar( 'second-footer-widget-area' ) ) : ?>
+				<div id="second" class="widget-area second">
+
+						<?php dynamic_sidebar( 'second-footer-widget-area' ); ?>
+
+				</div><!-- #second .widget-area -->
+<?php endif; ?>
+
+<?php if ( is_active_sidebar( 'third-footer-widget-area' ) ) : ?>
+				<div id="third" class="widget-area third">
+
+						<?php dynamic_sidebar( 'third-footer-widget-area' ); ?>
+
+				</div><!-- #third .widget-area -->
+<?php endif; ?>
+
+<?php if ( is_active_sidebar( 'fourth-footer-widget-area' ) ) : ?>
+				<div id="fourth" class="widget-area fourth">
+
+						<?php dynamic_sidebar( 'fourth-footer-widget-area' ); ?>
+
+				</div><!-- #fourth .widget-area -->
+<?php endif;
