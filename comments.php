61b5427e2a04ac38ccf17eae63b7abcf35fffd5c
Hugo Ashmore <hugo@nettropia.co.uk>
2017-10-07 19:03:11 +0100

    Add $args array for comment form, update submit element token for `button`.

--- comments.php
+++ comments.php
@@ -1,20 +1,20 @@
 <?php
 /**
- * The template for displaying comments
- *
- * This is the template that displays the area of the page that contains both the current comments
- * and the comment form.
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
- *
- * @package BP_Progenitor
- */
+	* The template for displaying comments
+	*
+	* This is the template that displays the area of the page that contains both the current comments
+	* and the comment form.
+	*
+	* @link https://codex.wordpress.org/Template_Hierarchy
+	*
+	* @package BP_Progenitor
+	*/
 
 /*
- * If the current post is protected by a password and
- * the visitor has not yet entered the password we will
- * return early without loading the comments.
- */
+	* If the current post is protected by a password and
+	* the visitor has not yet entered the password we will
+	* return early without loading the comments.
+	*/
 if ( post_password_required() ) {
 	return;
 }
@@ -65,8 +65,19 @@ if ( post_password_required() ) {
 		endif;
 
 	endif; // Check for have_comments().
-
-	comment_form();
+	$args = array(
+		'id_form'           => 'commentform',
+		'class_form'      => 'comment-form',
+		'id_submit'         => 'submit',
+		'class_submit'      => 'submit button',
+		'name_submit'       => 'Reply',
+		'title_reply'       => __( 'Leave a Reply' ),
+		'title_reply_to'    => __( 'Leave a Reply to %s' ),
+		'cancel_reply_link' => __( 'Cancel Reply' ),
+		'label_submit'      => __( 'Post Comment' ),
+		'format'            => 'xhtml',
+		);
+	comment_form( $args );
 	?>
 
 </div><!-- #comments -->
