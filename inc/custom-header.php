967c109b491ec8b1341292a8025d6aff4b65ead4
Hugo Ashmore <hugo@nettropia.co.uk>
2017-10-11 22:34:45 +0100

    Add default custom header image call.
    Add default image file.
    
    Add responsive widths for WP video widget  to prevent it breaking fluid layouts with over large fixed inline widths.

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -18,7 +18,7 @@
  */
 function bp_progenitor_custom_header_setup() {
 	add_theme_support( 'custom-header', apply_filters( 'bp_progenitor_custom_header_args', array(
-		'default-image'          => get_template_directory_uri() . '/images/header/default-background.png',
+		'default-image'          => get_template_directory_uri() . '/assets/images/default-images/default-header.jpg',
 		'default-text-color'     => '000000',
 		'width'                  => 1400,
 		'height'                 => 350,
