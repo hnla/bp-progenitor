d13b1cf8a0423315e2274e92e3b250a2f7d47a52
Hugo Ashmore <hugo@nettropia.co.uk>
2017-11-25 18:43:13 +0000

    Update main wp post loop include
    Add pagination functionality.

--- template-parts/home-page/home-page-post-loop.php
+++ template-parts/home-page/home-page-post-loop.php
@@ -10,25 +10,19 @@ if ( have_posts() ) : ?>
 
 	<div class="<?php post_loops_wrap_class(); ?>">
 
-	<?php /* Start the Loop */
-	while ( have_posts() ) : the_post();
-
-		/*
-		 * Include the Post-Format-specific template for the content.
-		 * If you want to override this in a child theme, then include a file
-		 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-		 */ ?>
+	<?php
+	while ( have_posts() ) : the_post(); ?>
 		<div class="box <?php echo ( is_sticky() )? ' sticky-post' : ''; ?>">
-			<?php get_template_part( 'template-parts/content-parts/content', get_post_format() ); ?>
+			<?php get_template_part( 'template-parts/content-parts/content' ); ?>
 		</div>
 
 	<?php endwhile; ?>
 
 	</div>
 
-	<?php if ( ! is_front_page() ) {
-		bp_progenitor_post_navigation( array('prev_text' => 'Older posts', 'next_text' => 'Newer Posts', 'icons' => true) );
-	};
+
+	<?php		progenitor_postloop_paginate();
+
 
 else :
 
